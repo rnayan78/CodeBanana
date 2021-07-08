320d846cd3edbc50288ff06d1041c1af3cabce03
rnayan78 <56041285+rnayan78@users.noreply.github.com>
2021-07-08 22:44:10 +0530

    Update Data.js

--- src/components/pages/HomePage/Data.js
+++ src/components/pages/HomePage/Data.js
@@ -5,7 +5,7 @@ export const homeObjOne = {
   topLine: "Explore CodeBanana", // - working correctly
   headline: "Learn to code in a new way",
   description:
-    "Our awesome coding platform lets you visualize code like you’ve never seen before",
+    "Our awesome coding platform lets you visualize code like you’ve never seen before.",
   buttonLabel: "Get Started",
   imgStart: "",
   img: "images/2.svg", // need to change the image
@@ -19,7 +19,7 @@ export const homeObjTwo = {
   topLine: "EASY SETUP",
   headline: "Super Fast and Easy to use",
   description:
-    "Start coding with a click of a button. The no installation feature ensures that you have access anywhere",
+    "Start coding with a click of a button. The no installation feature ensures that you have access anytime, anywhere.",
   imgStart: "start",
   img: "images/3.svg",
   alt: "Vault",
@@ -32,7 +32,7 @@ export const homeObjThree = {
   topLine: "Build Projects",
   headline: "Build actual working projects",
   description:
-    "Build user-friendly projects. Learn concepts while building",
+    "Build user-friendly projects. Get your hands dirty because that is the best way to learn.",
   imgStart: "",
   img: "images/1.svg",
   alt: "Vault",
@@ -45,7 +45,7 @@ export const homeObjFour = {
   topLine: "Certificates",
   headline: "Once you’re done building, get certified",
   description:
-    "Although we believe your projects are enough to show your skills, a little showing-off never hurt anyone",
+    "Although we believe your projects are enough to show your skills, a little showing-off never hurt anyone.",
   imgStart: "start",
   img: "images/4.svg",
   alt: "Vault",
