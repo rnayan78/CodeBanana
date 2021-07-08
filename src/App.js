6373764d9b4b56d84f4edb32173ce841cb8456fa
rnayan78 <56041285+rnayan78@users.noreply.github.com>
2021-07-08 13:33:52 +0530

    Fixed Navigation
    Changed the path to Projects when the get started button is clicked

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Home from './components/pages/HomePage/Home';
 import Projects from './components/pages/Projects/Projects';
 
 import SignUp from './components/pages/SignUp/SignUp';
-import contactUs from './components/pages/ContactUs/ContactUs';
+import ContactUs from './components/pages/ContactUs/ContactUs';
 
 
 function App() {
@@ -18,7 +18,7 @@ function App() {
         <Route path='/' exact component={Home}/>
         <Route path='/projects' component={Projects}/>
         <Route path='/contactUs' component={contactUs}/>
-        <Route path='/sign-up' component={SignUp}/>
+        <Route path='/sign-up' component={Projects}/>
         
       </Switch>
       <Footer />
