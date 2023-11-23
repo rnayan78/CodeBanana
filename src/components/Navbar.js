d42a9505db7c7da024bdd5176a5c2fdd4399ea88
AdityaBlackforge <adityapsingh@blockforgemines.com>
2023-11-23 15:34:00 +0530

    new changes

---
+++ src/components/Navbar.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
+import { Button } from "./Button";
+import { FaBars, FaTimes, FaLaptopCode } from "react-icons/fa";
+import { IconContext } from "react-icons/lib"; //Context Api of react
+import imgsrc from "./logo-v1.png";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    //function to show the button depending on screen ratio
+    if (window.innerWidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  window.addEventListener("resize", showButton);
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        {" "}
+        <div>{/* changes the color of the bars have to wrap it */}</div>
+        <div className="navbar">
+          <div className="navbar-container container">
+            <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+              <img
+                src={imgsrc}
+                className="navbar-icon"
+                alt="logo"
+                width="100"
+                height="100"
+              />
+              CodeBanana
+            </Link>
+            <div className="menu-icon" onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </div>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
+              <li className="nav-item">
+                <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/projects"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Projects
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/contactUs"
+                  className="nav-links"
+                  onClick={closeMobileMenu}
+                >
+                  Contact Us
+                </Link>
+              </li>
+              {/* <li className="nav-btn">
+                       {button  ? (
+                           <Link to='/sign-up' className="btn-link">
+                               <Button buttonStyle='btn--outline'>Sign Up</Button>
+                           </Link>
+                       ):(
+                           <Link to='/sign-up'  className="btn-link" onClick={closeMobileMenu}>
+                               <Button buttonStyle='btn--outline' buttonSize='btn-mobile'>Sign up</Button>
+                           </Link>
+                       )}
+                   </li> */}
+            </ul>
+          </div>
+        </div>
+      </IconContext.Provider>
+    </>
+  );
+}
+
+export default Navbar;
---
+++ src/components/pages/Footer/Footer.js
@@ -0,0 +1,113 @@
+import React from "react";
+import "./Footer.css";
+import { Button } from "../../Button";
+import { Link } from "react-router-dom";
+import { FaInstagram, FaTwitter, FaFacebook, FaMailBulk } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
+import imgsrc from "../../logo-v1.png";
+
+function Footer() {
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Subscribe to get notified about our next project
+        </p>
+        <p className="footer-subscription-text">
+          No spams, we promise
+        </p>
+        <div className="input-areas">
+          <form>
+            <input
+              className="footer-input"
+              name="email"
+              type="email"
+              placeholder="Your Email"
+            />
+          </form>
+          <a
+            href="https://forms.gle/bLGWYFmg8KiQzRQS7"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Button buttonStyle="btn--outline">Subscribe</Button>
+          </a>
+        </div>
+      </section>
+
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              <img
+                src={imgsrc}
+                className="navbar-icon"
+                alt="logo"
+                width="100"
+                height="100"
+              />
+              CodeBanana
+            </Link>
+          </div>
+          <small className="website-rights">CodeBanana © 2023</small>
+          <div className="social-icons">
+            {<Link
+              className='social-icon-link'
+              to='//www.facebook.com/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <FaFacebook />
+            </Link>}
+            <Link
+              className="social-icon-link"
+              to="//www.instagram.com/"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <FaInstagram />
+            </Link>
+
+            <Link
+              className='social-icon-link'
+              to='mailto:priambada.kumari@ieseg.fr' // Replace 'your-email@gmail.com' with your actual Gmail address
+              target='_blank'
+              aria-label='Gmail'
+            >
+              <FaMailBulk />
+              </Link>
+            {/*<Link      
+              className='social-icon-link'
+              to={                 {to direct it to a link {'//link'}}
+                '//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber'
+              }
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <FaYoutube />
+            </Link>*/}
+            <Link
+              className="social-icon-link"
+              to="//www.twitter.com/"
+              target="_blank"
+              aria-label="Twitter"
+            >
+              <FaTwitter />
+            </Link>
+            {/*<Link
+              className="social-icon-link"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn"
+            >
+              <FaLinkedin />
+          </Link>*/}
+
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
